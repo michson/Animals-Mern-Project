b94823e3298ce313b2aa5b54522a2488c7dea480
Olaoye Mich <41102062+michson@users.noreply.github.com>
2022-09-06 01:22:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -111,7 +111,6 @@ function ourCleanup(req,res,next){
     next()
 }
   
-
 async function start(){
     const client = new MongoClient("mongodb://root:root@localhost:27017/AmazingMernApp?&authSource=admin")
     await client.connect()
